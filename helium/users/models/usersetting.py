5508a9d7833262fa31edefdf2e3f77965d0bf02a
Alex Laird <alexdlaird@gmail.com>
2017-11-28 08:23:27 -0800

    Changing sub-user models to dynamically reference 'User' model.

--- helium/users/models/usersetting.py
+++ helium/users/models/usersetting.py
@@ -4,6 +4,7 @@ UserSetting model.
 
 import logging
 
+from django.conf import settings
 from django.db import models
 
 from helium.common import enums
@@ -19,4 +20,4 @@ logger = logging.getLogger(__name__)
 class UserSetting(BaseModel):
     time_zone = models.CharField(default='America/Los_Angeles', max_length=255, choices=enums.TIME_ZONE_CHOICES)
 
-    user = models.OneToOneField('User', related_name='settings', on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='settings', on_delete=models.CASCADE)
