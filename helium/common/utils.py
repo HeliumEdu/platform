45bf1e631c5e58fb9144e2562c4fbd5a0fc1e188
Alex Laird <alexdlaird@gmail.com>
2017-11-28 15:27:04 -0800

    Adding additional user profile and settings field, starting external_calendars model.

--- helium/common/utils.py
+++ helium/common/utils.py
@@ -5,9 +5,9 @@ Utility functions.
 import datetime
 import hashlib
 import sys
-from random import randint
+from random import randint, choice
 
-import requests
+from django.conf import settings
 from django.contrib.auth import get_user_model
 
 __author__ = 'Alex Laird'
@@ -15,14 +15,13 @@ __copyright__ = 'Copyright 2017, Helium Edu'
 __version__ = '1.0.0'
 
 
-def get_status_code(url, allow_redirects=False):
-    response = requests.head(url, allow_redirects=allow_redirects)
-
-    return response.status_code
-
-
-def remove_non_alnum(dirty_str):
-    return unicode(''.join(e for e in dirty_str.lower() if e.isalnum()))
+def generate_random_color(used=()):
+    color = choice(settings.ALLOWED_COLORS)
+    # Once we've used all allowed colors, we have to duplicate, but until then try to be unique
+    if len(used) < len(settings.ALLOWED_COLORS):
+        while color in used:
+            color = choice(settings.ALLOWED_COLORS)
+    return color
 
 
 def generate_verification_code():
@@ -33,6 +32,18 @@ def generate_verification_code():
         code = hashlib.sha1(str(seed_text) + datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S%f')).hexdigest()
         # Ensure the slug does not already exist in the database
         if get_user_model().objects.filter(verification_code=code).exists():
+            code = None
+
+    return code
+
+
+def generate_phone_verification_code():
+    code = None
+    while not code:
+        code = randint(100000, 999999)
+
+        # Ensure the slug does not already exist in the database
+        if get_user_model().objects.filter(profile__phone_verification_code=code).exists():
             code = None
 
     return code
