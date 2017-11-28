792c8b7b22fdd4be426d0985ada9ba87a316f8ca
Alex Laird <contact@alexlaird.com>
2017-11-27 19:38:52 -0800

    Adding UserSettings model. Fixing UserVoice widget.

--- helium/users/models/__init__.py
+++ helium/users/models/__init__.py
@@ -1,1 +1,2 @@
 from user import User
+from usersetting import UserSetting
---
+++ helium/users/models/usersetting.py
@@ -0,0 +1,38 @@
+"""
+UserSetting model.
+"""
+
+import logging
+
+from django.db import models
+
+from helium.common import enums
+from helium.common.models.base import BaseModel
+
+__author__ = 'Alex Laird'
+__copyright__ = 'Copyright 2017, Helium Edu'
+__version__ = '1.0.0'
+
+logger = logging.getLogger(__name__)
+
+
+class UserSetting(BaseModel):
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+
+    last_name = models.CharField(max_length=30, blank=True, null=True)
+
+    address_1 = models.CharField(max_length=255, blank=True, null=True)
+
+    address_2 = models.CharField(max_length=255, blank=True, null=True)
+
+    city = models.CharField(max_length=255, blank=True, null=True)
+
+    state = models.CharField(choices=enums.STATE_CHOICES, max_length=2, blank=True, null=True)
+
+    postal_code = models.CharField(max_length=255, blank=True, null=True)
+
+    country = models.CharField(max_length=255, blank=True, null=True, default='United States')
+
+    phone = models.CharField(max_length=255, blank=True, null=True)
+
+    time_zone = models.CharField(default='America/Chicago', max_length=255, choices=enums.TIME_ZONE_CHOICES)
