19cb6cb17eafc80a7a707073d96ccb22869418b3
Alex Laird <alexdlaird@gmail.com>
2018-01-23 20:36:53 -0800

    Migrating basic skeleton implementation of grades page.

--- helium/planner/static/js/grades/main.js
+++ helium/planner/static/js/grades/main.js
@@ -41,6 +41,42 @@ function HeliumGrades() {
             $("#plot-tooltip").hide();
         }
     };
+
+    /**
+     * Add the given course group's data to the page.
+     *
+     * @param data the data for the course group to be added
+     */
+    this.add_course_group_to_page = function (data) {
+        if (helium.data_has_err_msg(data)) {
+            helium.ajax_error_occurred = true;
+            $("#loading-grades").spin(false);
+
+            bootbox.alert(data[0].err_msg);
+        } else {
+            if (data.shown_on_calendar) {
+                $("#course-group-tabs").append('<li><a data-toggle="tab" href="#course-group-container-' + data.id + '"><i class="icon-book r-110"></i><span class="hidden-xs">' + data.title + '</span></a></li>');
+                var container = $('<div id="course-group-container-' + data.id + '" class="tab-pane"></div>');
+                var details = $('<div id="details-for-course-group-' + data.id + '" class="col-sm-12 infobox-container">');
+                container.append(details);
+                $("#course-group-tab-content").append(container);
+
+                // TODO: need to get these from the backend
+                var percent_thru = 100;
+                var days_remaining = 20;
+                details.append('<div class="infobox infobox-blue2">' + percent_thru + '<div class="infobox-progress"><div class="easy-pie-chart percentage easyPieChart" data-percent="' + (percent_thru < 100 ? percent_thru : 100) + '" data-size="46" style="width: 46px; height: 46px; line-height: 46px;"> <span class="percent">' + (percent_thru < 100 ? percent_thru : 100) + '</span>% <canvas width="46" height="46"></canvas></div></div><div class="infobox-data"><span class="infobox-text">thru term</span><div class="infobox-content">' + days_remaining + ' days remaining</div></div>');
+                var total_assignments = 20;
+                details.append('<div class="infobox infobox-red"><div class="infobox-icon"><i class="icon-beaker"></i></div><div class="infobox-data"><span class="infobox-data-number">' + total_assignments + '</span><div class="infobox-content">assignments</div></div></div>');
+                var num_graded = 10;
+                details.append('<div class="infobox infobox-red"><div class="infobox-icon"><i class="icon-beaker"></i></div><div class="infobox-data"><span class="infobox-data-number">' + num_graded + '</span><div class="infobox-content">graded</div></div></div>');
+                var num_complete = 10;
+                var num_incomplete = 5;
+                details.append('<div class="infobox infobox-blue2">' + percent_thru + '<div class="infobox-progress"><div class="easy-pie-chart percentage easyPieChart" data-percent="' + percent_thru + '" data-size="46" style="width: 46px; height: 46px; line-height: 46px;"><span class="percent">' + percent_thru + '</span>%<canvas width="46" height="46"></canvas></div></div><div class="infobox-data"><span class="infobox-text">' + num_complete + ' complete</span><div class="infobox-content">' + num_incomplete + ' remaining</div></div></div><div class="space-20"></div></div>');
+                container.append('<div class="row"><div class="col-xs-12"><div class="widget-box transparent"><div class="widget-header widget-header-flat"><h4 class="lighter"><i class="icon-signal"></i>Grades for ' + data.title + '</h4><a class="cursor-hover" data-action="collapse"><div class="widget-toolbar"><span class="badge badge-info">' + (data.average_grade != -1 ? (data.average_grade + '% ' + (data.trend > 0 ? '<span class="icon-x arrow-up-icon light-green"></span>' : '<span class="icon-x arrow-down-icon light-red"></span>')) : 'N/A') + '</span><i class="icon-chevron-up"></i></div></a></div><div class="widget-body"><div class="widget-main"><div id="course-group-chart-' + data.id + '" style="width: 100%; height: 220px; padding: 0px; position: relative;"> <canvas class="flot-base" width="942" height="220" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 942px; height: 220px;"></canvas> <div class="flot-text" style="position: absolute; top: 0px; left: 0px; bottom: 0px; right: 0px; font-size: smaller; color: rgb(84, 84, 84);"><div class="flot-y-axis flot-y1-axis yAxis y1Axis" style="position: absolute; top: 0px; left: 0px; bottom: 0px; right: 0px; display: block;"><div class="flot-tick-label tickLabel" style="position: absolute; top: 190px; left: 7px; text-align: right;">50%</div><div class="flot-tick-label tickLabel" style="position: absolute; top: 152px; left: 7px; text-align: right;">60%</div><div class="flot-tick-label tickLabel" style="position: absolute; top: 114px; left: 7px; text-align: right;">70%</div><div class="flot-tick-label tickLabel" style="position: absolute; top: 76px; left: 7px; text-align: right;">80%</div><div class="flot-tick-label tickLabel"style="position: absolute; top: 38px; left: 7px; text-align: right;">90%</div><div class="flot-tick-label tickLabel"style="position: absolute; top: 0px; left: 1px; text-align: right;">100%</div></div></div><canvas class="flot-overlay" width="942" height="220" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 942px; height: 220px;"></canvas><div class="legend"><div style="position: absolute; width: 185px; height: 120px; top: 13px; right: 23px; opacity: 0.85; background-color: rgb(255, 255, 255);"></div><table style="position:absolute;top:13px;right:23px;;font-size:smaller;color:#545454"><tbody></tbody></table></div></div></div></div></div></div></div>');
+                container.append('<div class="row"><div class="col-xs-12" id="course-group-piechart-' + data.id + '"></div></div>');
+            }
+        }
+    };
 }
 
 // Initialize HeliumGrades and give a reference to the Helium object
@@ -88,6 +124,14 @@ $(document).ready(function () {
     /*******************************************
      * Other page initialization
      ******************************************/
+    helium.planner_api.get_course_groups(function (data) {
+        $.each(data, function (i, course_group_data) {
+            helium.grades.add_course_group_to_page(course_group_data);
+        });
+    }, false);
+
+    $($("#course-group-tabs li a").first()).tab("show");
+
     helium.planner_api.get_grades(function (data) {
         helium.ajax_error_occurred = false;
         if (helium.data_has_err_msg(data)) {
@@ -96,10 +140,22 @@ $(document).ready(function () {
 
             bootbox.alert(data[0].err_msg);
         } else {
-            helium.grades.course_groups = data.course_groups;
-            helium.grades.courses_for_course_group = data.courses_for_course_group;
-            helium.grades.categories_for_course = data.categories_for_course;
-            helium.grades.grade_points_for_course_group = data.grade_points_for_course_group;
+            helium.grades.course_groups = {};
+            helium.grades.courses_for_course_group = {};
+            helium.grades.grade_points_for_course_group = {};
+            $.each(data.course_groups, function (i, course_group) {
+                helium.grades.course_groups[course_group['id']] = course_group;
+                helium.grades.courses_for_course_group[course_group['id']] = course_group['courses'];
+
+                helium.grades.grade_points_for_course_group[course_group['id']] = {};
+
+                helium.grades.categories_for_course = {};
+                $.each(course_group['courses'], function (i, course) {
+                    helium.grades.categories_for_course[course['id']] = course['categories'];
+
+                    helium.grades.grade_points_for_course_group[course_group['id']][course['id']] = course['grade_points'];
+                });
+            });
 
             $('<div id="plot-tooltip"></div>').css({
                 position: "absolute",
