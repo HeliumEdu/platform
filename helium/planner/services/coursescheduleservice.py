0f5ce068b1cc62c26423707a3c592bd4aa129890
Alex Laird <alexdlaird@gmail.com>
2018-03-14 20:04:17 -0700

    Beginning to implement caching of course schedules (implementation incomplete and in-memory cache needs better testing support).

--- helium/planner/services/coursescheduleservice.py
+++ helium/planner/services/coursescheduleservice.py
@@ -1,11 +1,14 @@
 import datetime
+import json
 import logging
 
 import pytz
+from django.core.cache import cache
 from django.utils.timezone import make_aware
 
 from helium.common import enums
 from helium.planner.models import Event
+from helium.planner.serializers.eventserializer import EventSerializer
 
 __author__ = 'Alex Laird'
 __copyright__ = 'Copyright 2018, Helium Edu'
@@ -71,34 +74,48 @@ def course_schedules_to_events(course, course_schedules):
     """
     events = []
 
+    cache_prefix = "{}:{}:courseschedule:".format(course.get_user().pk, course.pk)
+
     # TODO: responses should, in the future, be cached for at least a few minutes
-    day = course.start_date
-    while day <= course.end_date:
-        for course_schedule in course_schedules.iterator():
-            if course_schedule.days_of_week[enums.PYTHON_TO_HELIUM_DAY_OF_WEEK[day.weekday()]] == "1":
-                start_time = __get_start_time_for_weekday(course_schedule,
+    # TODO: when a class is saved, clear all cached keys with this prefix
+    cached_events = cache.get_many(cache.keys(cache_prefix + "*")) if getattr(cache, "keys", None) else None
+    if cached_events:
+        for event in cached_events:
+            serializer = EventSerializer(data=event)
+
+            events.append(serializer.data)
+    else:
+        day = course.start_date
+        while day <= course.end_date:
+            for course_schedule in course_schedules.iterator():
+                if course_schedule.days_of_week[enums.PYTHON_TO_HELIUM_DAY_OF_WEEK[day.weekday()]] == "1":
+                    start_time = __get_start_time_for_weekday(course_schedule,
+                                                              enums.PYTHON_TO_HELIUM_DAY_OF_WEEK[day.weekday()])
+                    end_time = __get_end_time_for_weekday(course_schedule,
                                                           enums.PYTHON_TO_HELIUM_DAY_OF_WEEK[day.weekday()])
-                end_time = __get_end_time_for_weekday(course_schedule,
-                                                      enums.PYTHON_TO_HELIUM_DAY_OF_WEEK[day.weekday()])
-                comments = __get_comments(course)
+                    comments = __get_comments(course)
 
-                event = Event(id=len(events),
-                              title=course.title,
-                              all_day=False,
-                              show_end_time=True,
-                              start=make_aware(datetime.datetime.combine(day, start_time),
-                                               pytz.timezone(course.get_user().settings.time_zone)).astimezone(
-                                  pytz.utc),
-                              end=make_aware(datetime.datetime.combine(day, end_time),
-                                             pytz.timezone(course.get_user().settings.time_zone)).astimezone(pytz.utc),
-                              owner_id=course.pk,
-                              user=course.get_user(),
-                              calendar_item_type=enums.COURSE,
-                              comments=comments)
-                events.append(event)
+                    event = Event(id=len(events),
+                                  title=course.title,
+                                  all_day=False,
+                                  show_end_time=True,
+                                  start=make_aware(datetime.datetime.combine(day, start_time),
+                                                   pytz.timezone(course.get_user().settings.time_zone)).astimezone(
+                                      pytz.utc),
+                                  end=make_aware(datetime.datetime.combine(day, end_time),
+                                                 pytz.timezone(course.get_user().settings.time_zone)).astimezone(
+                                      pytz.utc),
+                                  owner_id=course.pk,
+                                  user=course.get_user(),
+                                  calendar_item_type=enums.COURSE,
+                                  comments=comments)
+                    events.append(event)
 
-                break
+                    serializer = EventSerializer(event)
+                    cache.set(cache_prefix + str(event.id), json.dumps(serializer.data))
 
-        day += datetime.timedelta(days=1)
+                    break
+
+            day += datetime.timedelta(days=1)
 
     return events
