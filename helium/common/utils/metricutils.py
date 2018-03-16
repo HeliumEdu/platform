77f3442ab754f8d5d0e10f04337cbad1b689cc8d
Alex Laird <alexdlaird@gmail.com>
2018-03-16 12:29:30 -0700

    Removing numeric characters from URL metric IDs.

--- helium/common/utils/metricutils.py
+++ helium/common/utils/metricutils.py
@@ -1,3 +1,4 @@
+import re
 from statsd.defaults.django import statsd
 
 __author__ = 'Alex Laird'
@@ -16,7 +17,7 @@ def increment(metric, request=None, ignore_staff=True, ignore_anonymous=False):
 
 
 def request_start(request):
-    metric_id = "platform.{}.{}".format(request.path.replace('/', ''), request.method)
+    metric_id = "platform.{}.{}".format(re.sub("[^a-zA-Z]+", "", request.path), request.method)
     timer = statsd.timer(metric_id + '.timer', rate=1)
     timer.start()
 
