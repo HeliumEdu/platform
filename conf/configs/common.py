9ed792270d7dcf42e04d38cede5848891bb2a3a1
Alex Laird <alexdlaird@gmail.com>
2018-04-03 15:34:17 -0700

    Bumping versions to Django 2.0.4 and DRF 3.8.0.

--- conf/configs/common.py
+++ conf/configs/common.py
@@ -72,7 +72,6 @@ DEFAULT_MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'maintenance_mode.middleware.MaintenanceModeMiddleware',
