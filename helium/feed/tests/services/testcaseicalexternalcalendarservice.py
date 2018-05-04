38657411fc29841fa4c089e7ef1a58a294c9aabf
Alex Laird <alexdlaird@gmail.com>
2018-05-04 12:25:56 -0700

    Adding security concern with urlopen and local files.

---
+++ helium/feed/tests/services/testcaseicalexternalcalendarservice.py
@@ -0,0 +1,20 @@
+import logging
+
+from django.test import TestCase
+
+from helium.feed.services import icalexternalcalendarservice
+from helium.feed.services.icalexternalcalendarservice import HeliumICalError
+
+__author__ = "Alex Laird"
+__copyright__ = "Copyright 2018, Helium Edu"
+__version__ = '1.4.9'
+
+logger = logging.getLogger(__name__)
+
+
+class TestCaseICALExternalCalendarService(TestCase):
+    def test_validate_url_file_invalid(self):
+        # GIVEN
+        with self.assertRaises(HeliumICalError) as ctx:
+            icalexternalcalendarservice.validate_url("file://some_local_file.txt")
+        self.assertEqual(str(ctx.exception), 'Enter a valid URL.')
