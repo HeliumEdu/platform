8ba8805c4de0763bfbd14e1f566faddc6b4ab4bf
Alex Laird <alexdlaird@gmail.com>
2018-06-14 18:37:09 -0700

    [heliumcli] Release 1.4.23

--- conf/configs/common.py
+++ conf/configs/common.py
@@ -11,7 +11,7 @@ from conf.settings import PROJECT_ID
 
 __author__ = 'Alex Laird'
 __copyright__ = 'Copyright 2018, Helium Edu'
-__version__ = '1.4.22'
+__version__ = '1.4.23'
 
 # ############################
 # Project configuration
