28dc78d836538faaffe2470e9d73b678763e1397
Alex Laird <alexdlaird@gmail.com>
2018-07-01 20:25:42 -0700

    Removing sitemaps dependency, as now handled by frontend.

--- conf/configs/common.py
+++ conf/configs/common.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'django.contrib.sitemaps',
     # Health modules
     'health_check',
     'health_check.db',
