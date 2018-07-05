ebede01849638ae5812f18126a1fda46b62bb6d1
Alex Laird <alexdlaird@gmail.com>
2018-07-05 10:29:56 -0700

    Adding test case for time_zone when getting user info. Adding test case for /info.

---
+++ helium/common/tests/views/apis/testcaseinfoviews.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+__author__ = 'Alex Laird'
+__copyright__ = 'Copyright 2018, Helium Edu'
+__version__ = '1.4.23'
+
+
+class TestCaseInfoViews(APITestCase):
+    def test_info(self):
+        # WHEN
+        response = self.client.get(reverse('resource_info'))
+
+        # THEN
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(settings.PROJECT_NAME, response.data['name'])
+        self.assertEqual(settings.PROJECT_TAGLINE, response.data['tagline'])
+        self.assertEqual(settings.PROJECT_VERSION, response.data['version'])
+        self.assertEqual(settings.EMAIL_ADDRESS, response.data['support_email'])
+        self.assertEqual(settings.SUPPORT_REDIRECT_URL, response.data['support_url'])
+        self.assertEqual(settings.MAX_UPLOAD_SIZE, response.data['max_upload_size'])
