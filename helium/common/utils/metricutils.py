4e74729fcaf8b3b56b2125b3c34dac0f617ea31d
Alex Laird <alexdlaird@gmail.com>
2019-08-07 09:18:40 -0700

    [heliumcli] Release 1.4.43

--- helium/common/utils/metricutils.py
+++ helium/common/utils/metricutils.py
@@ -23,7 +23,7 @@ if settings.DATADOG_API_KEY:
 
 __author__ = "Alex Laird"
 __copyright__ = "Copyright 2019, Helium Edu"
-__version__ = "1.4.38"
+__version__ = "1.4.43"
 
 
 def increment(metric, request=None, ignore_staff=True, ignore_anonymous=False):
