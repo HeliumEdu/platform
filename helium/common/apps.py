5a339f58a3b4e15160cd5595339a4e1832ac2504
Alex Laird <alexdlaird@gmail.com>
2020-03-28 11:07:45 -0700

    Adding ngrok support.

--- helium/common/apps.py
+++ helium/common/apps.py
@@ -1,6 +1,9 @@
+import sys
+
 from celery import current_app
 from django.apps import AppConfig
 from django.conf import settings
+from urllib.parse import urlparse
 
 from health_check.plugins import plugin_dir
 
@@ -14,6 +17,8 @@ class CommonConfig(AppConfig):
     verbose_name = 'Common'
 
     def ready(self):
+        self.init_ngrok()
+
         plugin_dir.reset()
 
         from health_check.db.backends import DatabaseBackend
@@ -50,3 +55,21 @@ class CommonConfig(AppConfig):
                                          'critical': False,
                                          'description': 'Attachment and other file storage'
                                      }))
+
+    def init_ngrok(self):
+        if settings.DEV_SERVER and settings.USE_NGROK:
+            # pyngrok will only be installed, and should only ever be initialized, in a dev environment
+            from pyngrok import ngrok
+
+            # Get the dev server port (defaults to 8000 for Django, can be overridden with the
+            # last arg when calling `runserver`)
+            addrport = urlparse('http://{}'.format(sys.argv[-1]))
+            port = addrport.port if addrport.netloc and addrport.port else 8000
+
+            # Open a ngrok tunnel to the dev server
+            public_url = ngrok.connect(port).rstrip("/")
+            print('ngrok tunnel "{}" -> "http://127.0.0.1:{}/"'.format(public_url, port))
+
+            # Update any base URLs or webhooks to use the public ngrok URL
+            settings.PROJECT_HOST = public_url
+            CommonConfig.init_webhooks(public_url)
