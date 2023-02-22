94c34bde562da68e0222f2045d1ee88ac9ab78e1
Alex Laird <contact@alexlaird.com>
2023-02-21 20:22:51 -0600

    Add correction for DST time in iCal calendars and class schedules. Add tests.

--- helium/feed/services/icalexternalcalendarservice.py
+++ helium/feed/services/icalexternalcalendarservice.py
@@ -86,12 +86,16 @@ def _create_events_from_calendar(external_calendar, calendar):
                 start = datetime.datetime.combine(start, datetime.time.min)
             if timezone.is_naive(start):
                 start = timezone.make_aware(start, time_zone)
+                if start.dst():
+                    start = (start + datetime.timedelta(hours=1))
             start = start.astimezone(pytz.utc)
 
             if all_day:
                 end = datetime.datetime.combine(end, datetime.time.min)
             if timezone.is_naive(end):
                 end = timezone.make_aware(end, time_zone)
+                if end.dst():
+                    end = (end + datetime.timedelta(hours=1))
             end = end.astimezone(pytz.utc)
 
             event = Event(id=len(events),
