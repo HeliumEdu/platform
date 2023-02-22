6208250ff9be4e4392b9bd3c3948b93e996b8c3f
Alex Laird <contact@alexlaird.com>
2023-02-21 20:33:22 -0600

    [heliumcli] Release 1.4.50

--- helium/feed/tests/views/apis/testcaseexternalresourceviews.py
+++ helium/feed/tests/views/apis/testcaseexternalresourceviews.py
@@ -13,8 +13,8 @@ from helium.feed.tests.helpers import externalcalendarhelper
 from helium.feed.tests.helpers import icalfeedhelper
 
 __author__ = "Alex Laird"
-__copyright__ = "Copyright 2020, Helium Edu"
-__version__ = "1.4.45"
+__copyright__ = "Copyright 2023, Helium Edu"
+__version__ = "1.4.50"
 
 
 class TestCaseExternalCalendarResourceViews(APITestCase, CacheTestCase):
--- helium/planner/tests/views/apis/testcasecoursescheduleresourceviews.py
+++ helium/planner/tests/views/apis/testcasecoursescheduleresourceviews.py
@@ -11,8 +11,8 @@ from helium.planner.models import CourseSchedule
 from helium.planner.tests.helpers import coursegrouphelper, coursehelper, courseschedulehelper
 
 __author__ = "Alex Laird"
-__copyright__ = "Copyright 2021, Helium Edu"
-__version__ = "1.4.46"
+__copyright__ = "Copyright 2023, Helium Edu"
+__version__ = "1.4.50"
 
 
 class TestCaseCourseScheduleResourceViews(APITestCase, CacheTestCase):
