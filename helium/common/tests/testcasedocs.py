83c72e00431500fa9cbb65ea6edd0b236f20a34a
Alex Laird <alaird@twilio.com>
2023-03-18 14:10:57 -0500

    Add schema so django-rest-framework continues to generate coreAPI docs. Add test for /docs so it doesn't break again.

---
+++ helium/common/tests/testcasedocs.py
@@ -0,0 +1,22 @@
+import os
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from helium.auth.tests.helpers import userhelper
+from helium.planner.models import Attachment
+from helium.planner.tests.helpers import coursegrouphelper, coursehelper, attachmenthelper, eventhelper, homeworkhelper
+
+__author__ = "Alex Laird"
+__copyright__ = "Copyright 2023, Helium Edu"
+__version__ = "1.4.46"
+
+
+class TestCaseDocs(APITestCase):
+    def test_docs(self):
+        # WHEN
+        response = self.client.get('/docs/')
+
+        # THEN
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
