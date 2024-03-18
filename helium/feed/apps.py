6b7ab6ba48f3ea6e7192567731b141dc0ffc3167
Alex Laird <contact@alexlaird.com>
2024-03-18 11:31:40 -0500

    Default auto field improvements for schema migration.

--- helium/feed/apps.py
+++ helium/feed/apps.py
@@ -8,4 +8,4 @@ from django.apps import AppConfig
 class FeedConfig(AppConfig):
     name = 'helium.feed'
     verbose_name = 'Feed'
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.AutoField'
