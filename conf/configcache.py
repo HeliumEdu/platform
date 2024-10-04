684cd5c571fe2e23698de34237fdcb823d39bccc
Alex Laird <contact@alexlaird.com>
2024-10-03 21:12:59 -0500

    Log private IP of Farget host to ALLOWED_HOSTS. Don't log ALLOWED_HOSTS to Rollbar, too noisy.

--- conf/configcache.py
+++ conf/configcache.py
@@ -5,13 +5,14 @@ import botocore.session
 from aws_secretsmanager_caching import SecretCache, SecretCacheConfig
 from decouple import config as decouple_config
 
+secret_manager_secret_name = os.environ.get("PLATFORM_AWS_SECRET_MANAGER_SECRET_NAME")
 aws_secrets = {}
-if os.environ.get("PLATFORM_AWS_SECRET_MANAGER_SECRET_NAME"):
+if secret_manager_secret_name:
     client = botocore.session.get_session().create_client('secretsmanager',
                                                           'us-east-1')
     cache_config = SecretCacheConfig()
     cache = SecretCache(config=cache_config, client=client)
-    secret_str = cache.get_secret_string(os.environ.get('PLATFORM_AWS_SECRET_MANAGER_SECRET_NAME'))
+    secret_str = cache.get_secret_string(secret_manager_secret_name)
     aws_secrets = json.loads(secret_str)
 
 
