5bb711cf8aeedfdd233e95c20a3db326582746f8
Alex Laird <contact@alexlaird.com>
2024-10-15 12:29:03 -0500

    Create provision-dot-env.sh, which can be used by the deploy pipeline to run ci-tests against a local Docker instance.

---
+++ bin/provision-dot-env.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+PARENT_PATH=$(dirname "${BASH_SOURCE[0]}")
+DOT_ENV_PATH="$PARENT_PATH/../.env"
+
+cp -n "$PARENT_PATH/../.env.docker.example" "$DOT_ENV_PATH" | true
+
+declare -a VARS=("PLATFORM_EMAIL_HOST_USER"
+  "PLATFORM_EMAIL_HOST_PASSWORD"
+  "PLATFORM_TWILIO_ACCOUNT_SID"
+  "PLATFORM_TWILIO_AUTH_TOKEN"
+  "PLATFORM_TWILIO_SMS_FROM")
+
+echo "Provisioning .env file with variables from environment (if defined): [${VARS[*]}] ..."
+
+for var in "${VARS[@]}"
+do
+  if [ -n "${var}" ]; then
+    echo "--> Updating $var"
+    gsed -i "/^$var=/c\\$var=${!var}" "$DOT_ENV_PATH"
+  fi
+done
