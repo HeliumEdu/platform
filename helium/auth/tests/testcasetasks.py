8cae2f6628d4b76c5776526ed6bdd630010a89c9
Alex Laird <contact@alexlaird.com>
2025-08-28 16:58:02 -0500

    Add purge task that removes users that never finish setting up their account after 7 days.

--- helium/auth/tests/testcasetasks.py
+++ helium/auth/tests/testcasetasks.py
@@ -6,12 +6,14 @@ import json
 from datetime import datetime, timedelta
 
 import pytz
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
-from helium.auth.tasks import expire_auth_tokens
+from helium.auth.tasks import expire_auth_tokens, purge_unverified_users
 from helium.auth.tests.helpers import userhelper
 
 
@@ -54,3 +56,28 @@ class TestCaseTasks(APITestCase):
         self.assertEqual(Token.objects.count(), 3)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + response.data['token'])
         self.assertEqual(self.client.get(reverse('auth_user_detail')).status_code, status.HTTP_200_OK)
+
+    def test_purge_unverified_users(self):
+        # GIVEN
+        user1 = userhelper.given_a_user_exists()
+        user1.created_at = datetime.now().replace(tzinfo=pytz.utc) - timedelta(days=settings.UNVERIFIED_USER_TTL_DAYS, minutes=1)
+        user1.save()
+        user2 = userhelper.given_an_inactive_user_exists(username='user2', email='test2@email.com')
+        user2.created_at = datetime.now().replace(tzinfo=pytz.utc) - timedelta(days=settings.UNVERIFIED_USER_TTL_DAYS) + timedelta(minutes=1)
+        user2.save()
+        user3 = userhelper.given_an_inactive_user_exists(username='user3', email='test3@email.com')
+        user3.created_at = datetime.now().replace(tzinfo=pytz.utc) - timedelta(days=settings.UNVERIFIED_USER_TTL_DAYS, minutes=1)
+        user3.save()
+        user4 = userhelper.given_an_inactive_user_exists(username='user4', email='test4@email.com')
+        user4.created_at = datetime.now().replace(tzinfo=pytz.utc) - timedelta(days=settings.UNVERIFIED_USER_TTL_DAYS, minutes=1)
+        user4.save()
+        self.assertEqual(get_user_model().objects.count(), 4)
+
+        # WHEN
+        purge_unverified_users()
+
+        # THEN
+        users = get_user_model().objects.all()
+        self.assertEqual(len(users), 2)
+        self.assertEqual(users[0].pk, user1.pk)
+        self.assertEqual(users[1].pk, user2.pk)
