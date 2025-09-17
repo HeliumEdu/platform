0781b56d5bd00f2efbf850f822549aa5e97b087a
Alex Laird <contact@alexlaird.com>
2025-09-17 16:52:36 -0500

    Fix tests

--- helium/auth/tests/testcasetasks.py
+++ helium/auth/tests/testcasetasks.py
@@ -13,7 +13,7 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
-from helium.auth.tasks import expire_auth_tokens, purge_unverified_users
+from helium.auth.tasks import purge_unverified_users, expire_and_blacklist_auth_tokens
 from helium.auth.tests.helpers import userhelper
 
 
@@ -35,7 +35,7 @@ class TestCaseTasks(APITestCase):
         self.assertEqual(self.client.get(reverse('auth_user_detail')).status_code, status.HTTP_200_OK)
 
         # WHEN
-        expire_auth_tokens()
+        expire_and_blacklist_auth_tokens()
 
         # THEN
         self.assertEqual(Token.objects.count(), 2)
@@ -60,16 +60,20 @@ class TestCaseTasks(APITestCase):
     def test_purge_unverified_users(self):
         # GIVEN
         user1 = userhelper.given_a_user_exists()
-        user1.created_at = datetime.now().replace(tzinfo=pytz.utc) - timedelta(days=settings.UNVERIFIED_USER_TTL_DAYS, minutes=1)
+        user1.created_at = datetime.now().replace(tzinfo=pytz.utc) - timedelta(days=settings.UNVERIFIED_USER_TTL_DAYS,
+                                                                               minutes=1)
         user1.save()
         user2 = userhelper.given_an_inactive_user_exists(username='user2', email='test2@email.com')
-        user2.created_at = datetime.now().replace(tzinfo=pytz.utc) - timedelta(days=settings.UNVERIFIED_USER_TTL_DAYS) + timedelta(minutes=1)
+        user2.created_at = datetime.now().replace(tzinfo=pytz.utc) - timedelta(
+            days=settings.UNVERIFIED_USER_TTL_DAYS) + timedelta(minutes=1)
         user2.save()
         user3 = userhelper.given_an_inactive_user_exists(username='user3', email='test3@email.com')
-        user3.created_at = datetime.now().replace(tzinfo=pytz.utc) - timedelta(days=settings.UNVERIFIED_USER_TTL_DAYS, minutes=1)
+        user3.created_at = datetime.now().replace(tzinfo=pytz.utc) - timedelta(days=settings.UNVERIFIED_USER_TTL_DAYS,
+                                                                               minutes=1)
         user3.save()
         user4 = userhelper.given_an_inactive_user_exists(username='user4', email='test4@email.com')
-        user4.created_at = datetime.now().replace(tzinfo=pytz.utc) - timedelta(days=settings.UNVERIFIED_USER_TTL_DAYS, minutes=1)
+        user4.created_at = datetime.now().replace(tzinfo=pytz.utc) - timedelta(days=settings.UNVERIFIED_USER_TTL_DAYS,
+                                                                               minutes=1)
         user4.save()
         self.assertEqual(get_user_model().objects.count(), 4)
 
