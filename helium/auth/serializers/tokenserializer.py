4df1fff083a1b376c9802e116f12b17f2ee3b685
Alex Laird <contact@alexlaird.com>
2025-09-20 18:13:06 -0500

    A few more metrics.

--- helium/auth/serializers/tokenserializer.py
+++ helium/auth/serializers/tokenserializer.py
@@ -10,6 +10,8 @@ from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.settings import api_settings
 
+from helium.common.utils import metricutils
+
 logger = logging.getLogger(__name__)
 
 
@@ -47,6 +49,7 @@ class TokenSerializer(TokenObtainPairSerializer):
             attrs["access"] = str(refresh.access_token)
 
             if api_settings.UPDATE_LAST_LOGIN or self.context.get('request').data.get('last_login_now', False):
+                metricutils.increment('action.user.login')
                 update_last_login(None, user)
 
         return attrs
