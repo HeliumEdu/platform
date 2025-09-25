4dcedc0e2e37109e513b76c8761cfdabf6819e8c
Alex Laird <contact@alexlaird.com>
2025-09-25 16:19:33 -0500

    Fix metric formatting.

--- helium/common/utils/metricutils.py
+++ helium/common/utils/metricutils.py
@@ -23,7 +23,7 @@ def increment(metric, request=None, response=None, value=1, extra_tags=None):
 
     if request:
         tags.extend(
-            [f"method:{request.method}", f"authenticated:{str(request.user.is_authenticated).lower()}"])
+            [f"method:{request.method.upper()}", f"authenticated:{str(request.user.is_authenticated).lower()}"])
         if request.user.is_authenticated:
             tags.append(f"staff:{str(request.user.is_staff).lower()}")
         if 'User-Agent' in request.headers:
@@ -31,18 +31,16 @@ def increment(metric, request=None, response=None, value=1, extra_tags=None):
     if response:
         tags.append(f"status_code:{response.status_code}")
 
-    logger.warning(f"EMIT METRIC {metric} WITH {tags}")
-
     statsd.increment(f"platform.{metric}", value=value, tags=tags)
 
 
 def request_start(request):
-    metric_id = f"platform.request.{re.sub('[^a-zA-Z]+', '', request.path)}"
+    metric_id = f"request.{re.sub('[^a-zA-Z]+', '', request.path)}"
 
     return {
         'Request-Metric-ID': metric_id,
         'Request-Metric-Start': int(round(time.time() * 1000)),
-        'Request-Method': request.method
+        'Request-Method': request.method.upper()
     }
 
 
@@ -51,7 +49,7 @@ def request_stop(metrics, response):
 
     increment(metrics['Request-Metric-ID'], response=response,
               extra_tags=[f"method:{metrics['Request-Method']}"])
-    statsd.timing(metrics['Request-Metric-ID'] + ".time", metrics['Request-Metric-Millis'], tags=DATADOG_BASE_TAGS)
+    statsd.timing(f"platform.{metrics['Request-Metric-ID']}.time", metrics['Request-Metric-Millis'], tags=DATADOG_BASE_TAGS)
 
     metrics.pop('Request-Method', None)
     for name, value in metrics.items():
@@ -59,7 +57,7 @@ def request_stop(metrics, response):
 
 
 def task_start(task_name):
-    metric_id = f"platform.task.{task_name}"
+    metric_id = f"task.{task_name}"
 
     return {
         'Task-Metric-ID': metric_id,
@@ -71,4 +69,4 @@ def task_stop(metrics):
     metrics['Request-Metric-Millis'] = int(time.time() * 1000) - metrics['Task-Metric-Start']
 
     increment(metrics['Task-Metric-ID'])
-    statsd.timing(metrics['Task-Metric-ID'] + ".time", metrics['Request-Metric-Millis'], tags=DATADOG_BASE_TAGS)
+    statsd.timing(f"platform.{metrics['Task-Metric-ID']}.time", metrics['Request-Metric-Millis'], tags=DATADOG_BASE_TAGS)
