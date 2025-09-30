5a63616071cc952eebb4a10d4a41014ac44a7f93
Alex Laird <contact@alexlaird.com>
2025-09-30 14:55:32 -0500

    Update support redirect URL for tests.

--- conf/configs/common.py
+++ conf/configs/common.py
@@ -172,7 +172,7 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 HOSTNAME = socket.gethostname()
 
-SUPPORT_REDIRECT_URL = "https://github.com/HeliumEdu/platform/wiki"
+SUPPORT_REDIRECT_URL = "https://github.com/HeliumEdu/platform/issues"
 BUG_REPORT_REDIRECT_URL = "https://github.com/HeliumEdu/platform/issues/new?assignees=&labels=bug&projects=&template=bug-report.yml"
 
 # Healthcheck
