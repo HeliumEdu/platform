3313f834f40878b8e8845d36609fcdb91e6403e6
Alex Laird <contact@alexlaird.com>
2025-10-08 07:35:34 -0500

    Dependency upgrades. Remove legacy /health endpoint.

--- helium/common/views/apis/statusviews.py
+++ helium/common/views/apis/statusviews.py
@@ -4,10 +4,8 @@ __version__ = "1.11.60"
 
 import copy
 import sys
-import time
 from concurrent.futures import ThreadPoolExecutor
 
-import psutil
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy as _
@@ -15,7 +13,6 @@ from django.views.decorators.cache import never_cache
 from rest_framework import status
 from rest_framework.viewsets import GenericViewSet
 
-from health_check.contrib.psutil.backends import MemoryUsage, DiskUsage
 from health_check.plugins import plugin_dir
 from helium.common.views.views import HeliumAPIView
 
@@ -80,37 +77,6 @@ class StatusResourceView(GenericViewSet, HeliumAPIView):
         return JsonResponse(
             {
                 "components": components,
-                "status": system_status
-            },
-            status=status_code
-        )
-
-
-class HealthResourceView(GenericViewSet, HeliumAPIView):
-    @method_decorator(never_cache)
-    def health(self, request, *args, **kwargs):
-        """
-        Check the health of this node and its dependencies.
-        """
-        plugins = sorted((
-            plugin_class(**copy.deepcopy(options))
-            for plugin_class, options in plugin_dir._registry
-        ), key=lambda plugin: plugin.identifier())
-        plugins.append(DiskUsage())
-        plugins.append(MemoryUsage())
-
-        errors = _run_checks(plugins)
-
-        status_code = status.HTTP_500_INTERNAL_SERVER_ERROR if errors else status.HTTP_200_OK
-
-        components, system_status = _build_components_status(plugins)
-
-        return JsonResponse(
-            {
-                "components": components,
-                "uptime": round(time.time() - psutil.boot_time(), 2),
-                "disk_usage": f"{psutil.disk_usage('/').percent}%",
-                "memory_available": f'{int(psutil.virtual_memory().available / 1024 / 1024)} MB',
                 "status": system_status
             },
             status=status_code
