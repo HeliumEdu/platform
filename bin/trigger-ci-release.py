e87f637c34bd24a5e3280a52b2e6dfe1669d32dd
Alex Laird <contact@alexlaird.com>
2025-10-23 18:35:05 -0500

    Wait for deploy to be live, then trigger cluster tests against dev.

--- bin/trigger-ci-release.py
+++ bin/trigger-ci-release.py
@@ -3,6 +3,7 @@
 import json
 import os
 import sys
+import time
 
 import requests
 
@@ -14,6 +15,8 @@ if not ENVIRONMENT or not TERRAFORM_API_TOKEN:
     print(
         "ERROR: Set all required env vars: ENVIRONMENT, TERRAFORM_API_TOKEN.")
     sys.exit(1)
+
+INFO_URI = "https://{}.heliumedu.com/info".format("api" if ENVIRONMENT == "prod" else f"api.{ENVIRONMENT}")
 
 # TODO: migrate this Terraform deployment code in to `heliumcli`, then it can be removed from deploy and platform
 
@@ -42,3 +45,33 @@ response = requests.post(f"https://app.terraform.io/api/v2/runs",
                              }}}).encode()).json()
 
 # In `dev` where we do CI, auto-apply is enabled, so no need to wait for plan here
+
+#####################################################################
+# Wait for the Terraform apply to be live
+#####################################################################
+
+current_version = None
+version_is_live = False
+retries = 0
+retry_sleep_seconds = 20
+wait_minutes = 10
+while retries < ((wait_minutes * 60) / retry_sleep_seconds):
+    result = requests.get(INFO_URI).json()
+    if current_version and result["version"] != current_version:
+        version_is_live = True
+        break
+    else:
+        if not current_version:
+            current_version = result["version"]
+
+        retries += 1
+        print(f"Waiting for {ENVIRONMENT} to report version {VERSION} ...")
+        time.sleep(retry_sleep_seconds)
+
+if version_is_live:
+    print(f"... {VERSION} is now live in {ENVIRONMENT}.")
+else:
+    print(
+        f"ERROR: {ENVIRONMENT} has not updated its version number to {VERSION} within {wait_minutes} minutes, "
+        "check if deploy failed.")
+    sys.exit(1)
