66ee6307ed6fb95a72362a8e257828513d83a3a4
Alex Laird <contact@alexlaird.com>
2025-11-05 23:38:39 -0600

    Update colors to jewel tones.

--- helium/auth/models/usersettings.py
+++ helium/auth/models/usersettings.py
@@ -30,15 +30,15 @@ class UserSettings(BaseModel):
 
     events_color = models.CharField(
         help_text='A valid hex color code choice to determine the color events will be shown on the calendar.',
-        max_length=7, validators=[validate_hex_color], default='#87b8d4')
+        max_length=7, validators=[validate_hex_color], default='#e74674')
 
     grade_color = models.CharField(
         help_text='A valid hex color code choice to determine the color grade badges will be.',
-        max_length=7, validators=[validate_hex_color], default='#a387d4')
+        max_length=7, validators=[validate_hex_color], default='#9d629d')
 
     material_color = models.CharField(
         help_text='A valid hex color code choice to determine the color material badges will be.',
-        max_length=7, validators=[validate_hex_color], default='#b8d487')
+        max_length=7, validators=[validate_hex_color], default='#cfa25e')
 
     default_reminder_offset = models.PositiveIntegerField(help_text='The default offset when creating a new reminder.',
                                                           default=30)
