c998d5f4e2d54dd3b41c8c0a805eaf9b3d063665
Alex Laird <contact@alexlaird.com>
2025-11-11 09:18:04 -0600

    Add endpoint to re-import example schedule.

--- helium/importexport/urls.py
+++ helium/importexport/urls.py
@@ -10,7 +10,10 @@ from helium.importexport.views.apis.importviews import ImportResourceView
 urlpatterns = [
     # Import/Export URLs
     path('importexport/import/', ImportResourceView.as_view({'post': 'import_data'}),
-         name='importexport_resource_import'),
+         name='importexport_import'),
     path('importexport/export/', ExportResourceView.as_view({'get': 'export_data'}),
-         name='importexport_resource_export'),
+         name='importexport_export'),
+
+    path('importexport/import/exampleschedule/', ImportResourceView.as_view({'post': 'import_exampleschedule'}),
+         name='importexport_import_exampleschedule'),
 ]
