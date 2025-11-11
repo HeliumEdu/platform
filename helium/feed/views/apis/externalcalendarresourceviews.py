e6e12cc14e4c974ed0acc2fe8333053bf80a5a2c
Alex Laird <contact@alexlaird.com>
2025-11-11 09:33:29 -0600

    Fix test issue

--- helium/feed/views/apis/externalcalendarresourceviews.py
+++ helium/feed/views/apis/externalcalendarresourceviews.py
@@ -7,12 +7,13 @@ from datetime import timezone
 
 from dateutil import parser
 from drf_spectacular.utils import extend_schema, OpenApiParameter
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError, NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from helium.common.permissions import IsOwner
 from helium.common.views.views import HeliumAPIView
+from helium.feed.models import ExternalCalendar
 from helium.feed.services import icalexternalcalendarservice
 from helium.feed.services.icalexternalcalendarservice import HeliumICalError
 from helium.planner.serializers.eventserializer import EventSerializer
@@ -23,6 +24,13 @@ logger = logging.getLogger(__name__)
 class ExternalCalendarAsEventsResourceView(HeliumAPIView):
     serializer_class = EventSerializer
     permission_classes = (IsAuthenticated, IsOwner,)
+
+    # def get_queryset(self):
+    #     if hasattr(self.request, 'user') and not getattr(self, "swagger_fake_view", False):
+    #         user = self.request.user
+    #         return user.external_calendars.all()
+    #     else:
+    #         return ExternalCalendar.objects.none()
 
     @extend_schema(
         parameters=[
@@ -38,7 +46,13 @@ class ExternalCalendarAsEventsResourceView(HeliumAPIView):
         The IDs given for each event are sequential, unique only amongst the results of this particular query, and not
         guaranteed to be consistent across calls.
         """
-        external_calendar = self.get_object()
+        try:
+            external_calendar = (ExternalCalendar.objects
+                                 .for_user(request.user.id)
+                                 .get(pk=self.kwargs['pk']))
+        except ExternalCalendar.DoesNotExist:
+            raise NotFound()
+
         start = parser.parse(request.query_params["start__gte"]).astimezone(
             timezone.utc) if "start__gte" in request.query_params else None
         end = parser.parse(request.query_params["end__lt"]).astimezone(
