366ac4c37600aad4c41be4d594cb9c4487eb33a6
Alex Laird <contact@alexlaird.com>
2025-12-13 16:02:35 -0600

    Add last mobile settings

--- helium/auth/serializers/usersettingsserializer.py
+++ helium/auth/serializers/usersettingsserializer.py
@@ -18,5 +18,6 @@ class UserSettingsSerializer(serializers.ModelSerializer):
             'time_zone', 'default_view', 'week_starts_on', 'all_day_offset', 'show_getting_started', 'events_color',
             'grade_color', 'material_color', 'default_reminder_offset', 'remember_filter_state',
             'calendar_event_limit', 'default_reminder_offset_type', 'calendar_use_category_colors',
-            'default_reminder_type', 'receive_emails_from_admin', 'private_slug', 'user',)
+            'default_reminder_type', 'receive_emails_from_admin', 'private_slug', 'mobile_default_view',
+            'default_mobile_reminder_type', 'default_mobile_reminder_offset_type', 'user',)
         read_only_fields = ('all_day_offset', 'private_slug', 'user',)
