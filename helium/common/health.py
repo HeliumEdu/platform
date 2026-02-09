2c72fc53513ae7a1ce7c0f1dd426745bb1d3ad41
Alex Laird <contact@alexlaird.com>
2026-02-09 13:39:22 -0600

    Revert "Update health.py"
    This reverts commit 4fe6547dd30559fafb3e689ab71fac240710c87c.

--- helium/common/health.py
+++ helium/common/health.py
@@ -5,17 +5,14 @@ __version__ = "1.17.8"
 from datetime import timedelta
 
 from celery import states
-from celery.exceptions import TaskRevokedError
-from django.conf import settings
 from django.utils import timezone
 from django_celery_results.models import TaskResult
 from health_check.backends import BaseHealthCheckBackend
 from health_check.cache.backends import CacheBackend
 from health_check.contrib.celery.backends import CeleryHealthCheck
-from health_check.contrib.celery.tasks import add
 from health_check.contrib.s3boto3_storage.backends import S3Boto3StorageHealthCheck
 from health_check.db.backends import DatabaseBackend
-from health_check.exceptions import HealthCheckException, ServiceUnavailable
+from health_check.exceptions import HealthCheckException
 
 
 class IdentifiedDatabaseBackend(DatabaseBackend):
@@ -34,30 +31,6 @@ class IdentifiedS3Boto3StorageHealthCheck(S3Boto3StorageHealthCheck):
 
 
 class IdentifiedCeleryHealthCheck(CeleryHealthCheck):
-    def check_status(self):
-        timeout = getattr(settings, 'HEALTHCHECK_CELERY_TIMEOUT', 3)
-
-        try:
-            for queue in self.queues:
-                result = add.apply_async(
-                    args=[4, 4],
-                    expires=timeout,
-                    queue=queue
-                )
-                result.get(timeout=timeout)
-                if result.result != 8:
-                    self.add_error(ServiceUnavailable(
-                        f"Celery returned wrong result for queue {queue}"))
-        except IOError as e:
-            self.add_error(ServiceUnavailable("IOError"), e)
-        except (TimeoutError, TaskRevokedError):
-            # Expected failures when workers are busy - report as unhealthy
-            # without logging exception traceback to Sentry
-            self.add_error(ServiceUnavailable(
-                "Celery workers busy or unresponsive"))
-        except BaseException as e:
-            self.add_error(ServiceUnavailable("Unknown error"), e)
-
     def identifier(self):
         return "TaskProcessing"
 
