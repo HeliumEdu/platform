0d5391f92405727eda07f0782d69bfce657f031c
Alex Laird <alaird@twilio.com>
2026-02-23 12:37:10 -0600

    Update pushservice.py

--- helium/common/services/pushservice.py
+++ helium/common/services/pushservice.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 def send_notifications(push_tokens, subject, message, reminder_data):
-    message = messaging.MulticastMessage(
+    multicast_message = messaging.MulticastMessage(
         notification=messaging.Notification(
             title=subject,
             body=message,
@@ -21,6 +21,16 @@ def send_notifications(push_tokens, subject, message, reminder_data):
         tokens=push_tokens
     )
 
-    messaging.send_each_for_multicast(message)
+    try:
+        response = messaging.send_each_for_multicast(multicast_message)
 
-    metricutils.increment('action.push.sent', value=len(push_tokens))
+        if response.success_count > 0:
+            metricutils.increment('action.push.sent', value=response.success_count)
+
+        if response.failure_count > 0:
+            logger.warning(f"Failed to send {response.failure_count} push notifications")
+            metricutils.increment('action.push.failed', value=response.failure_count)
+    except Exception:
+        logger.error("Failed to send push notifications", exc_info=True)
+        metricutils.increment('action.push.failed', value=len(push_tokens))
+        raise
