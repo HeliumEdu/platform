1ce84828bc16596422623f1d3602481d52e36129
Alex Laird <contact@alexlaird.com>
2026-03-09 10:19:07 -0500

    Adjust schedule times with DST corrections
    When shifting Homework and Event instances to a new week, replace previous localtime/make_aware datetime construction with UTC-based replacements plus explicit DST offset corrections. The patch computes start/end DST corrections by comparing the original item's utcoffset to the target day's midday utcoffset, then applies those offsets to the target datetimes. This prevents incorrect hour shifts when moving items across DST boundaries and preserves original local time-of-day values. Reminder adjustments remain unchanged.

--- helium/importexport/services/importservice.py
+++ helium/importexport/services/importservice.py
@@ -345,17 +345,25 @@ def _adjust_schedule_relative_to(user, adjust_month):
             course = homework.course
             start_delta = (homework.start.date() - course.start_date).days
             end_delta = (homework.end.date() - course.start_date).days
-            local_start = timezone.localtime(homework.start)
-            local_end = timezone.localtime(homework.end)
             target_start = first_monday + datetime.timedelta(days=start_delta)
             target_end = first_monday + datetime.timedelta(days=end_delta)
+            start_dst_correction = (timezone.localtime(homework.start).utcoffset()
+                                    - timezone.localtime(target_start.replace(hour=12, minute=0, second=0, microsecond=0)).utcoffset())
+            end_dst_correction = (timezone.localtime(homework.end).utcoffset()
+                                  - timezone.localtime(target_end.replace(hour=12, minute=0, second=0, microsecond=0)).utcoffset())
             Homework.objects.filter(pk=homework.pk).update(
-                start=timezone.make_aware(datetime.datetime(
-                    target_start.year, target_start.month, target_start.day,
-                    local_start.hour, local_start.minute, 0, 0)),
-                end=timezone.make_aware(datetime.datetime(
-                    target_end.year, target_end.month, target_end.day,
-                    local_end.hour, local_end.minute, 0, 0)))
+                start=target_start.replace(
+                    hour=homework.start.time().hour,
+                    minute=homework.start.time().minute,
+                    second=0,
+                    microsecond=0,
+                    tzinfo=timezone.utc) + start_dst_correction,
+                end=target_end.replace(
+                    hour=homework.end.time().hour,
+                    minute=homework.end.time().minute,
+                    second=0,
+                    microsecond=0,
+                    tzinfo=timezone.utc) + end_dst_correction)
 
             adjust_reminder_times(homework.pk, homework.calendar_item_type)
 
@@ -372,17 +380,25 @@ def _adjust_schedule_relative_to(user, adjust_month):
                 .filter(example_schedule=True).iterator()):
             start_delta = (event.start.date() - first_monday.date()).days + events_delta
             end_delta = (event.end.date() - first_monday.date()).days + events_delta
-            local_start = timezone.localtime(event.start)
-            local_end = timezone.localtime(event.end)
             target_start = first_monday + datetime.timedelta(days=start_delta)
             target_end = first_monday + datetime.timedelta(days=end_delta)
+            start_dst_correction = (timezone.localtime(event.start).utcoffset()
+                                    - timezone.localtime(target_start.replace(hour=12, minute=0, second=0, microsecond=0)).utcoffset())
+            end_dst_correction = (timezone.localtime(event.end).utcoffset()
+                                  - timezone.localtime(target_end.replace(hour=12, minute=0, second=0, microsecond=0)).utcoffset())
             Event.objects.filter(pk=event.pk).update(
-                start=timezone.make_aware(datetime.datetime(
-                    target_start.year, target_start.month, target_start.day,
-                    local_start.hour, local_start.minute, 0, 0)),
-                end=timezone.make_aware(datetime.datetime(
-                    target_end.year, target_end.month, target_end.day,
-                    local_end.hour, local_end.minute, 0, 0)))
+                start=target_start.replace(
+                    hour=event.start.time().hour,
+                    minute=event.start.time().minute,
+                    second=0,
+                    microsecond=0,
+                    tzinfo=timezone.utc) + start_dst_correction,
+                end=target_end.replace(
+                    hour=event.end.time().hour,
+                    minute=event.end.time().minute,
+                    second=0,
+                    microsecond=0,
+                    tzinfo=timezone.utc) + end_dst_correction)
 
             adjust_reminder_times(event.pk, event.calendar_item_type)
 
