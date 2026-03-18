90623e7873bebb459a96746c3c5c542de65b1e7c
Alex Laird <contact@alexlaird.com>
2026-03-18 09:06:26 -0500

    Update authservice.py

--- helium/auth/services/authservice.py
+++ helium/auth/services/authservice.py
@@ -4,7 +4,8 @@ __license__ = "MIT"
 import logging
 
 from django.conf import settings
-from django.contrib.auth import get_user_model, update_last_login
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import update_last_login
 from django.core.cache import cache
 from django.db.models import Q
 from firebase_admin import auth as firebase_auth
