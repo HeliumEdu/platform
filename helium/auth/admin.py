44bc19509dc0e20abb0c42cc79eaa7abfb40bf32
Alex Laird <contact@alexlaird.com>
2026-03-19 18:14:02 -0500

    Update admin.py

--- helium/auth/admin.py
+++ helium/auth/admin.py
@@ -161,9 +161,9 @@ class UserAdmin(admin.UserAdmin, BaseModelAdmin):
     form = AdminUserChangeForm
     add_form = AdminUserCreationForm
 
-    list_display = ('email', 'created_at', 'last_login_legacy', 'last_activity', 'get_auth_type',
+    list_display = ('email', 'last_activity', 'get_auth_type',
                     'num_notes', 'num_course_groups', 'num_courses', 'num_homework', 'num_events',
-                    'num_attachments', 'num_external_calendars', 'is_active')
+                    'num_attachments', 'num_external_calendars', 'created_at', 'last_login_legacy', 'is_active')
     list_filter = ('is_active', 'profile__phone_verified', 'settings__default_view', 'settings__remember_filter_state',
                    'settings__calendar_event_limit', 'settings__default_reminder_type', 'settings__color_scheme_theme',
                    'settings__calendar_use_category_colors', OAuthProviderFilter, HasWeightedGradingFilter,
