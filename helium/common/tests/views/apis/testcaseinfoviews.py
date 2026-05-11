5129631f6aaee517c2dbd3734b57facd39b9f034
Alex Laird <contact@alexlaird.com>
2026-05-11 12:06:32 -0500

    Extend /info API with token TTLs, OAuth providers, import types

--- helium/common/tests/views/apis/testcaseinfoviews.py
+++ helium/common/tests/views/apis/testcaseinfoviews.py
@@ -19,3 +19,7 @@ class TestCaseInfoViews(APITestCase):
         self.assertEqual(settings.PROJECT_VERSION, response.data['version'])
         self.assertEqual(settings.EMAIL_ADDRESS, response.data['support_email'])
         self.assertEqual(settings.MAX_UPLOAD_SIZE, response.data['max_upload_size'])
+        self.assertEqual(settings.ACCESS_TOKEN_TTL_MINUTES, response.data['access_token_lifetime_minutes'])
+        self.assertEqual(settings.REFRESH_TOKEN_TTL_DAYS, response.data['refresh_token_lifetime_days'])
+        self.assertEqual(['google', 'apple'], response.data['oauth_providers'])
+        self.assertEqual(['json'], response.data['import_file_types'])
